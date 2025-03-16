cd13cd292822db18c85fb7f2535d80ecc96c8408
aisyah <aaiisyah2007@gmail.com>
2025-03-16 13:30:00 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul5_103022300116;
+
+public class HaloGeneric
+{
+    public void SapaUser<T>(T X)
+    {
+        Console.WriteLine("Halo user " + X);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Aisyah");
+
+        DataGeneric<string> data = new DataGeneric<string>("103022300116");
+        data.PrintData();
+    }
+}
+
